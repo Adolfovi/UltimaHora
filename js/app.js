db18a3f95f0608e029a264b55c59d2c8eff5060e
Adolfovi <dolfillo99@gmail.com>
2022-04-13 15:32:13 +0200

    Añadido javascript

---
+++ js/app.js
@@ -0,0 +1,17 @@
+let numero = 0;
+
+function timer() {
+    numero = numero + 1;
+    console.log(numero);
+    if (numero >= 100) {
+        location.reload();
+        
+    }
+
+
+}
+
+function activetimer() {
+    console.log('En esta consola se mostrará el temporizador de la página web, cuando llegue a 100 se reiniciará');
+    timeractivated = setInterval(timer, 1000);
+}
